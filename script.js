6d80c60ff26e2ba64b20d4fb4f058edae4c8435e
Urvisha Vara <varaurvi@gmail.com>
2025-02-17 15:51:32 +0530

    add object data

---
+++ script.js
@@ -0,0 +1,11 @@
+const containerData = [
+    {
+        id: 1,
+        title: "STARNSTYLE Polycotton 220 TC Floral Printed King Size Double Bedsheet with 2 Pillow Covers, (90 x 90 Inches,) Brown",
+        price: "199 Rs.",
+        discount: "15%",
+        deliveryTime: "2-3 business days",
+        serviceName: "Amazon Prime",
+        img: "https://m.media-amazon.com/images/I/51y0xHiBPeL.jpg",  
+    },
+]
